f956e06b98e5bf279145db4cf8ff0746094416fa
Husen <husen@softwareseni.com>
2021-11-04 14:00:54 +0700

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f127379c3d428a013d4772521f9dcc13'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '138931f7-45e6-45bd-a7fc-491a21ddb1c2'
+};

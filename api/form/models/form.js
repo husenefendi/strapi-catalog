b3545edb8145b594ea14462bf97c97478332278e
Husen <husen@softwareseni.com>
2021-11-11 09:48:39 +0700

    add form response

--- api/form/models/form.js
+++ api/form/models/form.js
@@ -7,14 +7,7 @@
 
 module.exports = {
     lifecycles: {
-        async beforeCreate(data) {
-            console.log({ before: data })
-        },
-        async beforeFind(params, populat) {
-            // console.log({ beforefind: { params, populat } })
-        },
         async afterFind(results, params, populate) {
-            // console.log({ afterFind: { results: results[0], params, populate } })
             return results.map(e => {
                 return e.content_string = JSON.stringify(e.Content)
             })
